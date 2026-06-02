5ab663737e868a529440030b2d75c4100a22003c
eunseo <eunseo061113@gmail.com>
2026-06-02 21:23:57 +0900

    Update 인게임 시계(HUD_TIME)_조은서.c

--- 인게임 시계(HUD_TIME)_조은서.c
+++ 인게임 시계(HUD_TIME)_조은서.c
@@ -1,3 +1,7 @@
+// 1. 리눅스 GCC에서 기만적인 POSIX 표준 함수(usleep 등)를 컴파일하기 위한 매크로 선언 (최상단 필수)
+#define _DEFAULT_SOURCE
+#define _XOPEN_SOURCE 500
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -99,3 +103,17 @@ int main() {
             else if (key == 'q' || key == 'Q') {
                 game_over = true;
             }
+        }
+
+        HUD_TIME(start_time, accumulated_time, is_running);
+
+        // 2. 에러 방지를 위한 표준 지연 처리 방식 적용 (0.2초)
+        struct timespec ts = {0, 200000000}; 
+        nanosleep(&ts, NULL);
+    }
+
+    printf("\e[1;1H\e[2J");
+    printf("게임 종료!\n");
+    printf("\e[?25h"); 
+    return 0;
+}
